0b93bfe0b8e47a9a7f3055511c968a94b1595dce
tysonking33 <90588335+tysonking33@users.noreply.github.com>
2022-03-02 16:08:46 +1030

    First commit
    This is the first commit

---
+++ WHAMbam/WHAMbam.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Wham bam thank you mam");
+	return (0);
+}
---
+++ moth needs flame/moth_needs_flame.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Please moth requires the flame");
+	return (0);
+}
